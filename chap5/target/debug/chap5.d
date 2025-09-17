8f08b0e0a21c0c16720c73aa4b9a41cc06ae6b97
TatsukiKodama8 <kodama_tatsuki@retail-ai.jp>
2025-09-17 21:22:15 +0900

    build: targetディレクトリをignoreする

--- chap5/target/debug/chap5.d
+++
@@ -1,1 +0,0 @@
-/Users/tatsukikodama/prac-rust/chap5/target/debug/chap5: /Users/tatsukikodama/prac-rust/chap5/src/main.rs
--- chap5/target/debug/deps/chap5-04c6287e0e8afb6c.d
+++
@@ -1,5 +0,0 @@
-/Users/tatsukikodama/prac-rust/chap5/target/debug/deps/chap5-04c6287e0e8afb6c.d: src/main.rs
-
-/Users/tatsukikodama/prac-rust/chap5/target/debug/deps/chap5-04c6287e0e8afb6c: src/main.rs
-
-src/main.rs:
